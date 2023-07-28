033c723256dbe08ace55f44c6afa6dd2664cb059
Cyril Picard <cyril@picard.ch>
2023-07-28 16:30:25 -0400

    Change name to radcompressor (pypi collision)

--- scripts/sample_map_conditions.py
+++ scripts/sample_map_conditions.py
@@ -8,95 +8,136 @@ import pyarrow as pa
 import pyarrow.dataset as ds
 import pyarrow.parquet as pq
 
-from radcomp import thermo
+from radcompressor import thermo
 
 
 parameters = {
-    'T_range': [170, 400],  # K
-    'Pr': [1, 100],
-    'm_in': [5e-2, 0.7],
-    'mach_tip': [5e-2, 2.5]
+    "T_range": [170, 400],  # K
+    "Pr": [1, 100],
+    "m_in": [5e-2, 0.7],
+    "mach_tip": [5e-2, 2.5],
 }
 
 
 @click.command()
-@click.option('--geometries', '-g', type=click.Path(exists=True, dir_okay=False))
-@click.option('--output', type=click.Path(file_okay=False), default='data', help="Folder where dataset subfolders will be created")
-@click.option('--output-row-size', default=5000)
-@click.option('--fluid', '-f', 'fluid_list', type=str, required=True, multiple=True)
-@click.option('--fluid-type', type=click.Choice(['coolprop', 'refprop']), default='coolprop')
-@click.option('--batch-size', '-b', default=20000)
-@click.option('--n-points', '-no', default=50, help="Number of operating points (Mf, Nrot)")
-@click.option('--n-inlet', '-ni', default=10, help="Number of inlet conditions (Pin, Tin)")
-def main(geometries, output, output_row_size, fluid_list, fluid_type, batch_size, n_points, n_inlet):
+@click.option("--geometries", "-g", type=click.Path(exists=True, dir_okay=False))
+@click.option(
+    "--output",
+    type=click.Path(file_okay=False),
+    default="data",
+    help="Folder where dataset subfolders will be created",
+)
+@click.option("--output-row-size", default=5000)
+@click.option("--fluid", "-f", "fluid_list", type=str, required=True, multiple=True)
+@click.option(
+    "--fluid-type", type=click.Choice(["coolprop", "refprop"]), default="coolprop"
+)
+@click.option("--batch-size", "-b", default=20000)
+@click.option(
+    "--n-points", "-no", default=50, help="Number of operating points (Mf, Nrot)"
+)
+@click.option(
+    "--n-inlet", "-ni", default=10, help="Number of inlet conditions (Pin, Tin)"
+)
+def main(
+    geometries,
+    output,
+    output_row_size,
+    fluid_list,
+    fluid_type,
+    batch_size,
+    n_points,
+    n_inlet,
+):
     """Sample conditions for the provided geometries"""
     # Prepare fluid and load to check if it exists
-    if fluid_type == 'coolprop':
+    if fluid_type == "coolprop":
         fld = {f: thermo.CoolPropFluid(f) for f in fluid_list}
-    elif fluid_type == 'refprop':
+    elif fluid_type == "refprop":
         fld = {f: thermo.RefpropFluid(f) for f in fluid_list}
 
     # Prepare rng
     rng = np.random.default_rng()
 
     geometries = pathlib.Path(geometries)
-    output_subfolder = pathlib.Path(output) / (os.path.splitext(geometries.parts[-1])[0] + "_map_tabular")
+    output_subfolder = pathlib.Path(output) / (
+        os.path.splitext(geometries.parts[-1])[0] + "_map_tabular"
+    )
     output_subfolder.mkdir(exist_ok=True)
 
     geom_ds = ds.dataset(geometries)
 
-    lb_mn = np.array([parameters['m_in'][0], parameters['mach_tip'][0]])
-    ub_mn = np.array([parameters['m_in'][1], parameters['mach_tip'][1]])
+    lb_mn = np.array([parameters["m_in"][0], parameters["mach_tip"][0]])
+    ub_mn = np.array([parameters["m_in"][1], parameters["mach_tip"][1]])
 
-    xx, yy = np.meshgrid(np.linspace(0, 1, num=n_points, endpoint=True),
-                         np.linspace(0, 1, num=n_points, endpoint=True))
+    xx, yy = np.meshgrid(
+        np.linspace(0, 1, num=n_points, endpoint=True),
+        np.linspace(0, 1, num=n_points, endpoint=True),
+    )
     grid = np.c_[xx.ravel(), yy.ravel()]
     X_nm = grid * (ub_mn - lb_mn) + lb_mn
 
     idx_offset = 0
 
-    for b in geom_ds.to_batches(batch_size=batch_size, columns=['geom_id']):
+    for b in geom_ds.to_batches(batch_size=batch_size, columns=["geom_id"]):
         geom_idx = b.to_pandas().index
         n_geom = len(geom_idx)
 
-        fluids = rng.choice(fluid_list, size=n_geom*n_inlet, replace=True)
-        T_triple, T_max, T_crit, P_crit = np.array([[fld[f].T_triple, fld[f].T_max, fld[f].T_crit, fld[f].P_crit] for f in fluids]).T
-        T_low = np.maximum(T_triple+30, parameters['T_range'][0])
-        T_high = np.minimum(T_max-50, parameters['T_range'][1])
+        fluids = rng.choice(fluid_list, size=n_geom * n_inlet, replace=True)
+        T_triple, T_max, T_crit, P_crit = np.array(
+            [
+                [fld[f].T_triple, fld[f].T_max, fld[f].T_crit, fld[f].P_crit]
+                for f in fluids
+            ]
+        ).T
+        T_low = np.maximum(T_triple + 30, parameters["T_range"][0])
+        T_high = np.minimum(T_max - 50, parameters["T_range"][1])
 
-        Teff = rng.uniform(low=T_low, high=T_high, size=n_geom*n_inlet)
+        Teff = rng.uniform(low=T_low, high=T_high, size=n_geom * n_inlet)
 
         Pmax = P_crit * (Teff > T_crit)
-        Pmax[Pmax == 0] = [fld[f].thermo_prop('TQ', t, 1).P - 1.1e-4 for t, f in zip(Teff[Pmax == 0], fluids[Pmax == 0])]
+        Pmax[Pmax == 0] = [
+            fld[f].thermo_prop("TQ", t, 1).P - 1.1e-4
+            for t, f in zip(Teff[Pmax == 0], fluids[Pmax == 0])
+        ]
         # 1.1e-4 added to avoid Coolprop issues
 
-        Pmax[Pmax > P_crit/3] = P_crit[Pmax > P_crit/3]/3
+        Pmax[Pmax > P_crit / 3] = P_crit[Pmax > P_crit / 3] / 3
 
-        Peff_r = parameters['Pr'][0] + (1-rng.power(5, len(Teff))) * (parameters['Pr'][1]-parameters['Pr'][0])
-        Peff = Pmax/Peff_r
+        Peff_r = parameters["Pr"][0] + (1 - rng.power(5, len(Teff))) * (
+            parameters["Pr"][1] - parameters["Pr"][0]
+        )
+        Peff = Pmax / Peff_r
 
-        [fld[f].thermo_prop('PT', p, t).P - 1.1e-4 for t, f, p in zip(Teff, fluids, Peff)]
+        [
+            fld[f].thermo_prop("PT", p, t).P - 1.1e-4
+            for t, f, p in zip(Teff, fluids, Peff)
+        ]
 
-        m_in = np.tile(X_nm[:, 0], n_geom*n_inlet)
-        mach_tip = np.tile(X_nm[:, 1], n_geom*n_inlet)
+        m_in = np.tile(X_nm[:, 0], n_geom * n_inlet)
+        mach_tip = np.tile(X_nm[:, 1], n_geom * n_inlet)
 
-        df = pd.DataFrame({
-            'geom_id': geom_idx.repeat(n_inlet*n_points**2),
-            'fluid': fluids.repeat(n_points**2),
-            'in_T': Teff.repeat(n_points**2),
-            'in_P': Peff.repeat(n_points**2),
-            'in_m_in0': m_in,
-            'in_mach_tip': mach_tip
-        })
-        df.index.name = 'cond_id'
+        df = pd.DataFrame(
+            {
+                "geom_id": geom_idx.repeat(n_inlet * n_points**2),
+                "fluid": fluids.repeat(n_points**2),
+                "in_T": Teff.repeat(n_points**2),
+                "in_P": Peff.repeat(n_points**2),
+                "in_m_in0": m_in,
+                "in_mach_tip": mach_tip,
+            }
+        )
+        df.index.name = "cond_id"
 
         df.index += idx_offset
         idx_offset = df.index.max() + 1
 
         table = pa.Table.from_pandas(df, preserve_index=True)
-        table_name = output_subfolder / f'data_{geom_idx.min()}-{geom_idx.max()}.parquet'
+        table_name = (
+            output_subfolder / f"data_{geom_idx.min()}-{geom_idx.max()}.parquet"
+        )
         pq.write_table(table, table_name, row_group_size=output_row_size)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- scripts/simulate.py
+++ scripts/simulate.py
@@ -12,37 +12,39 @@ from distributed.worker import thread_state
 from dask.distributed import Client
 
 
-from radcomp.compressor import Compressor
-from radcomp.condition import OperatingCondition
-from radcomp.geometry import Geometry
-from radcomp.thermo import CoolPropFluid
+from radcompressor.compressor import Compressor
+from radcompressor.condition import OperatingCondition
+from radcompressor.geometry import Geometry
+from radcompressor.thermo import CoolPropFluid
 
 
 out_meta = {
-    'cond_id': pd.Series(dtype=np.int64),
-    'geom_id': pd.Series(dtype=np.int64),
-    'fluid': pd.Series(dtype=str),
-    'calc_tip_speed': pd.Series(dtype=np.double),
-    'calc_n_rot': pd.Series(dtype=np.double),
-    'calc_m_f': pd.Series(dtype=np.double),
-    'comp_valid': pd.Series(dtype=bool),
-    'comp_error': pd.Series(dtype=str),
-    'comp_eta_tt': pd.Series(dtype=np.double),
-    'comp_pr': pd.Series(dtype=np.double),
-    'comp_m_in': pd.Series(dtype=np.double),
-    'comp_n_rot_corr': pd.Series(dtype=np.double),
-    'comp_flow': pd.Series(dtype=np.double),
-    'comp_head': pd.Series(dtype=np.double),
-    'comp_power': pd.Series(dtype=np.double),
-    'dtime': pd.Series(dtype=np.double)
+    "cond_id": pd.Series(dtype=np.int64),
+    "geom_id": pd.Series(dtype=np.int64),
+    "fluid": pd.Series(dtype=str),
+    "calc_tip_speed": pd.Series(dtype=np.double),
+    "calc_n_rot": pd.Series(dtype=np.double),
+    "calc_m_f": pd.Series(dtype=np.double),
+    "comp_valid": pd.Series(dtype=bool),
+    "comp_error": pd.Series(dtype=str),
+    "comp_eta_tt": pd.Series(dtype=np.double),
+    "comp_pr": pd.Series(dtype=np.double),
+    "comp_m_in": pd.Series(dtype=np.double),
+    "comp_n_rot_corr": pd.Series(dtype=np.double),
+    "comp_flow": pd.Series(dtype=np.double),
+    "comp_head": pd.Series(dtype=np.double),
+    "comp_power": pd.Series(dtype=np.double),
+    "dtime": pd.Series(dtype=np.double),
 }
 
 out_thermo_meta = out_meta.copy()
-out_thermo_meta.update({
-    'in0_mu': pd.Series(dtype=np.double),
-    'in0_rho': pd.Series(dtype=np.double),
-    'in0_A': pd.Series(dtype=np.double)
-})
+out_thermo_meta.update(
+    {
+        "in0_mu": pd.Series(dtype=np.double),
+        "in0_rho": pd.Series(dtype=np.double),
+        "in0_A": pd.Series(dtype=np.double),
+    }
+)
 
 out_meta = pd.DataFrame(out_meta)
 out_thermo_meta = pd.DataFrame(out_thermo_meta)
@@ -50,31 +52,35 @@ out_thermo_meta = pd.DataFrame(out_thermo_meta)
 
 def simulate(df, geom_file=None, add_thermo=False):
     df = df.reset_index(drop=False)
-    geom_t = pq.read_table(geom_file,
-                           filters=[('geom_id', '>=', df.geom_id.min()),
-                                    ('geom_id', '<=', df.geom_id.max())]).to_pandas()
+    geom_t = pq.read_table(
+        geom_file,
+        filters=[
+            ("geom_id", ">=", df.geom_id.min()),
+            ("geom_id", "<=", df.geom_id.max()),
+        ],
+    ).to_pandas()
 
     out_m = out_thermo_meta if add_thermo else out_meta
     out = {n: np.empty(len(df), dtype=t) for n, t in out_m.dtypes.items()}
 
-    out['cond_id'] = df.cond_id
-    out['geom_id'] = df.geom_id
-    out['fluid'] = df.fluid
+    out["cond_id"] = df.cond_id
+    out["geom_id"] = df.geom_id
+    out["fluid"] = df.fluid
 
     for row in df.itertuples():
         fld = CoolPropFluid(row.fluid)
         geom = Geometry.from_dict(geom_t.loc[row.geom_id, :].to_dict())
-        in0 = fld.thermo_prop('PT', row.in_P, row.in_T)
+        in0 = fld.thermo_prop("PT", row.in_P, row.in_T)
         if add_thermo:
-            out['in0_mu'][row.Index] = in0.V
-            out['in0_rho'][row.Index] = in0.D
-            out['in0_A'][row.Index] = in0.A
+            out["in0_mu"][row.Index] = in0.V
+            out["in0_rho"][row.Index] = in0.D
+            out["in0_A"][row.Index] = in0.A
         m_f = row.in_m_in0 * in0.A * in0.D * geom.A2_eff
         tip_speed = row.in_mach_tip * in0.A
         n_rot = tip_speed / geom.r4
-        out['calc_m_f'][row.Index] = m_f
-        out['calc_n_rot'][row.Index] = n_rot
-        out['calc_tip_speed'][row.Index] = tip_speed
+        out["calc_m_f"][row.Index] = m_f
+        out["calc_n_rot"][row.Index] = n_rot
+        out["calc_tip_speed"][row.Index] = tip_speed
 
         t0 = time.perf_counter()
         op = OperatingCondition(in0=in0, fld=fld, m=m_f, n_rot=n_rot)
@@ -84,21 +90,21 @@ def simulate(df, geom_file=None, add_thermo=False):
             dtime = time.perf_counter() - t0
         except Exception as e:
             dtime = time.perf_counter() - t0
-            out['comp_error'][row.Index] = repr(e)
-            out['comp_valid'][row.Index] = False
+            out["comp_error"][row.Index] = repr(e)
+            out["comp_valid"][row.Index] = False
         else:
-            out['comp_valid'][row.Index] = valid
-            out['comp_eta_tt'][row.Index] = comp.eff
-            out['comp_pr'][row.Index] = comp.PR
-            out['comp_m_in'][row.Index] = comp.m_in
-            out['comp_n_rot_corr'][row.Index] = comp.n_rot_corr
-            out['comp_flow'][row.Index] = comp.flow
-            out['comp_head'][row.Index] = comp.head
-            out['comp_power'][row.Index] = comp.power
-        out['dtime'][row.Index] = dtime
+            out["comp_valid"][row.Index] = valid
+            out["comp_eta_tt"][row.Index] = comp.eff
+            out["comp_pr"][row.Index] = comp.PR
+            out["comp_m_in"][row.Index] = comp.m_in
+            out["comp_n_rot_corr"][row.Index] = comp.n_rot_corr
+            out["comp_flow"][row.Index] = comp.flow
+            out["comp_head"][row.Index] = comp.head
+            out["comp_power"][row.Index] = comp.power
+        out["dtime"][row.Index] = dtime
 
     out_df = pd.DataFrame(out, columns=out_m.columns)
-    return out_df.set_index('cond_id')
+    return out_df.set_index("cond_id")
 
 
 # End general setup
@@ -106,19 +112,28 @@ initialize()
 
 
 @click.command()
-@click.option('--geometries', '-g', type=click.Path(exists=True, dir_okay=False), required=True)
-@click.option('--conditions', '-c', type=click.Path(exists=True, file_okay=False), required=True)
-@click.option('--output-npartitions', default=20)
-@click.option('--thermo/--no-thermo', default=False)
+@click.option(
+    "--geometries", "-g", type=click.Path(exists=True, dir_okay=False), required=True
+)
+@click.option(
+    "--conditions", "-c", type=click.Path(exists=True, file_okay=False), required=True
+)
+@click.option("--output-npartitions", default=20)
+@click.option("--thermo/--no-thermo", default=False)
 def main(geometries, conditions, output_npartitions, thermo):
     out_m = out_thermo_meta if thermo else out_meta
     c = Client()
-    output_name = conditions.replace('_tabular', '_output')
-    tab = dd.read_parquet(conditions, index='cond_id', split_row_groups=True)
-    out = tab.map_partitions(simulate, geom_file=geometries, add_thermo=thermo, meta=out_m.set_index('cond_id'))
+    output_name = conditions.replace("_tabular", "_output")
+    tab = dd.read_parquet(conditions, index="cond_id", split_row_groups=True)
+    out = tab.map_partitions(
+        simulate,
+        geom_file=geometries,
+        add_thermo=thermo,
+        meta=out_m.set_index("cond_id"),
+    )
     out.repartition(npartitions=output_npartitions).to_parquet(output_name)
     c.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
